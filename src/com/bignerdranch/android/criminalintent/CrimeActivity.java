7ce0c1b7fbe6721c298cda0e4bda7b6a9fb54170
Eugene Sia <eugene@eugenesia.co.uk>
2014-11-23 19:02:18 +0000

    Inherit CrimeActivity from FragmentActivity instead of ActionBarActivity

--- src/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ src/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -2,11 +2,11 @@ package com.bignerdranch.android.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.app.ActionBarActivity;
 
 
-public class CrimeActivity extends ActionBarActivity {
+public class CrimeActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
