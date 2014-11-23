47b53bce44f0f13f84b6d20a1dd7c2adaa354619
Eugene Sia <eugene@eugenesia.co.uk>
2014-11-23 14:58:45 +0000

    Use long date format

--- src/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ src/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -51,7 +52,7 @@ public class CrimeFragment extends Fragment {
         });
         
         mDateButton = (Button)v.findViewById(R.id.crime_date);
-        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setText(DateFormat.getLongDateFormat(getActivity()).format(mCrime.getDate()));
         mDateButton.setEnabled(false);
         
         mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
