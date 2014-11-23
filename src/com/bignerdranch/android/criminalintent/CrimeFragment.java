8241df3b19c707a9127d639994831b2fed4a77f7
Eugene Sia <eugene@eugenesia.co.uk>
2014-11-23 05:48:08 +0000

    Wiring up the EditText widget #7.9

--- src/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ src/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -2,13 +2,17 @@ package com.bignerdranch.android.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
+    private EditText mTitleField;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,24 @@ public class CrimeFragment extends Fragment {
         Bundle savedInstanceState) {
         
         View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+        
+        mTitleField = (EditText)v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            public void onTextChanged(
+                CharSequence c, int start, int before, int count) {
+                
+                mCrime.setTitle(c.toString());
+            }
+            
+            public void beforeTextChanged(
+                CharSequence c, int start, int count, int after) {
+                // This space intentionally left blank.
+            }
+            
+            public void afterTextChanged(Editable c) {
+                // This one too.
+            }
+        });
         return v;
     }
     
