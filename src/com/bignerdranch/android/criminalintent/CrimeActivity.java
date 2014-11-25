ce8118de3bb4748aea9ff73b4f1bb6c9d39d4b04
Eugene Sia <eugene@eugenesia.co.uk>
2014-11-25 00:42:00 +0000

    Remove unused imports

--- src/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ src/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -1,8 +1,6 @@
 package com.bignerdranch.android.criminalintent;
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 
 
 public class CrimeActivity extends SingleFragmentActivity {
