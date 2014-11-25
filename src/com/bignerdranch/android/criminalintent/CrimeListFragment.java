305bb3002fc0a7afff09a18fb7efd9c8c22dae8a
Eugene Sia <eugene@eugenesia.co.uk>
2014-11-25 01:11:54 +0000

    Overriding onListItemClick() to log Crime title #9.17

--- src/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ src/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class CrimeListFragment extends ListFragment {
     private ArrayList<Crime> mCrimes;
+    private static final String TAG = "CrimeListFragment";
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,12 @@ public class CrimeListFragment extends ListFragment {
             new ArrayAdapter<Crime>(getActivity(),
             android.R.layout.simple_list_item_1, mCrimes);
         setListAdapter(adapter);
+    }
+    
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Crime c = (Crime)(getListAdapter()).getItem(position);
+        Log.d(TAG, c.getTitle() + " was clicked");
     }
 
 }
