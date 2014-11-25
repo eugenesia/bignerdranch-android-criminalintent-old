5a8cb5b41c1b961d5ef763806c1a162183e3bc57
Eugene Sia <eugene@eugenesia.co.uk>
2014-11-25 00:47:22 +0000

    Setting up the ArrayAdapter #9.14

--- src/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ src/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.widget.ArrayAdapter;
 
 public class CrimeListFragment extends ListFragment {
     private ArrayList<Crime> mCrimes;
@@ -13,6 +14,11 @@ public class CrimeListFragment extends ListFragment {
         super.onCreate(savedInstanceState);
         getActivity().setTitle(R.string.crimes_title);
         mCrimes = CrimeLab.get(getActivity()).getCrimes();
+        
+        ArrayAdapter<Crime> adapter =
+            new ArrayAdapter<Crime>(getActivity(),
+            android.R.layout.simple_list_item_1, mCrimes);
+        setListAdapter(adapter);
     }
 
 }
