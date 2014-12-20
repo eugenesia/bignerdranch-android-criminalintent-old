ddd6800a4e40ee1f6a1bb849130d401f9a1416b4
Eugene Sia <eugene@eugenesia.co.uk>
2014-12-20 04:44:49 +0000

    Using newInstance(UUID) #10.6

--- src/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ src/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -7,6 +7,10 @@ public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        
+        UUID crimeId = (UUID)getIntent()
+            .getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
+        
+        return CrimeFragment.newInstance(crimeId);
     }
 }
