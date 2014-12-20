6efbb660683d8884798ff86c5f4b5a4249c6b858
Eugene Sia <eugene@eugenesia.co.uk>
2014-12-20 23:46:09 +0000

    Set up ViewPager #11.1

---
+++ src/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class CrimePagerActivity extends FragmentActivity {
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
